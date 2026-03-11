fde54c538de5127c7ba845a0716ae7b5d574d76e
Rachel-0727 <rxu@macalester.edu>
2026-03-10 20:34:09 -0500

    Added Posts Scaffolding (VERY MINIMALISTIC)

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_10_211509) do
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
