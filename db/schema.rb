4e957f3ac8c6ee241d1d355b3489313215179be9
Rachel-0727 <rxu@macalester.edu>
2026-03-25 20:24:12 -0500

    added user model, added test objects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_11_024055) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_26_010127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_11_024055) do
     t.date "event_date"
     t.string "event_title"
     t.string "location"
+    t.datetime "updated_at", null: false
+    t.string "username"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
   end
