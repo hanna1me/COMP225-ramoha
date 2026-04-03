4e2ee24fba68a95ea590bb29df16987f6aa8a46c
hanna1me <hchang@macalester.edu>
2026-04-03 13:35:12 -0500

    prof pic...

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   has_many :posts
+  has_one_attached :avatar
 end
