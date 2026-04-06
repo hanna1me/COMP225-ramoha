5a101aee8b02262f2fbd82369198b5c24f84cfe0
moyartumanley <mmanley@macalester.edu>
2026-04-06 01:02:32 -0500

    Unified UI

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,12 @@
 module PostsHelper
+  def event_countdown_text(event_date)
+    days_until = (event_date.to_date - Date.current).to_i
+    if days_until <=0
+      "This event has passed!"
+    elsif days_until > 30
+      "in 30+ days!"
+    else
+      "in #{pluralize(days_until, 'day')}!"
+    end
+  end
 end
