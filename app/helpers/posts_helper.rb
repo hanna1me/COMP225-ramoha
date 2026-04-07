d064fbf853afc7ef84062b31c5427980feb07d32
moyartumanley <mmanley@macalester.edu>
2026-04-06 23:44:33 -0500

    Helper for adding post dates

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,11 +2,19 @@ module PostsHelper
   def event_countdown_text(event_date)
     days_until = (event_date.to_date - Date.current).to_i
     if days_until <=0
-      "This event has passed!"
+      "this event has passed!"
     elsif days_until > 30
       "in 30+ days!"
     else
       "in #{pluralize(days_until, 'day')}!"
+    end
+  end
+
+  def format_date(event_date)
+    if event_date != nil
+      event_date.strftime("%B %d, %Y")
+    else
+      event_date
     end
   end
 end
