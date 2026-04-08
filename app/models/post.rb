3cd38747c431cfdcc7e1db1305456fdc88b27801
moyartumanley <mmanley@macalester.edu>
2026-04-08 18:43:41 -0500

    Added validations for post creation and username length, implemented basic visual error indicators

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-    belongs_to :organizer, class_name: 'User', foreign_key: "organizer_id"
+    belongs_to :organizer, class_name: "User", foreign_key: "organizer_id"
     has_many :requirements
-
     has_many :interestedevents
-    has_many :users, through: :interestedevents 
+    has_many :users, through: :interestedevents
+    validates :event_title, :location, :event_date, presence: true
 end
